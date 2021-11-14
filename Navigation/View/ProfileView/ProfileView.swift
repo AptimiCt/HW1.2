fa6e40c96151604b25c603f47870e65e10962aef
Vostrikov Aleksandr <aptimict11@gmail.com>
2021-11-14 23:31:10 +0300

    add ProfileView.swift, ProfileView.xib, image for foto, add elements on ProfileView.xib

---
+++ Navigation/View/ProfileView/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Александр Востриков on 14.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var fotoImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthDateLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+}

e10ac1197dc3a2d98bec02a42266734940a9b258
Vostrikov Aleksandr <aptimict11@gmail.com>
2021-11-14 23:57:31 +0300

    add ProfileViewController.swift, Profile.storyboard, Profile.storyboard is initial

---
+++ Navigation/Controller/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Александр Востриков on 14.11.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}

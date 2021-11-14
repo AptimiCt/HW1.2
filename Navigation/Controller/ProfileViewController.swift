a475ddf1e8fca46c4ed86dea2acf2678c6d127a6
Vostrikov Aleksandr <aptimict11@gmail.com>
2021-11-15 00:59:52 +0300

    Profile.storyboard is main storyboard, add view from xib on storyboard and change param UI elements

--- Navigation/Controller/ProfileViewController.swift
+++ Navigation/Controller/ProfileViewController.swift
@@ -12,7 +12,17 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let profileNibName = String(describing: ProfileView.self)
         
+        if let profileView = Bundle.main.loadNibNamed(profileNibName, owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 16, y: 44, width: view.bounds.width - 32, height: profileView.bounds.height)
+            
+            profileView.nameLabel.backgroundColor = .orange
+            profileView.birthDateLabel.backgroundColor = .green
+            profileView.cityLabel.backgroundColor = .purple
+        
+            view.addSubview(profileView)
+        }
     }
     
 
